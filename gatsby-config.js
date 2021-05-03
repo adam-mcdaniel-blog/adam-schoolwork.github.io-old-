3851b96cfd0f1ddb82bb7a4dec8e2d616528722d
Adam McDaniel <adam.mcdaniel17@gmail.com>
2021-05-03 07:29:42 -0400

    Fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
                 background_color: config.backgroundColor,
                 theme_color: config.themeColor,
                 display: 'minimal-ui',
-                icon: './static/favicon/favicon-512.png',
+                icon: './static/favicon/favicon.png',
             },
         },
         'gatsby-plugin-offline',
